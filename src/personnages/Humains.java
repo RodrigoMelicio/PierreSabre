abf0f340bf4b4c5f47db7dab88d0abebe59dbe17
RodrigoMelicio <rodrigo.fortes-melicio@univ-tlse3.fr>
2025-11-07 09:07:06 +0100

    Humains

---
+++ src/personnages/Humains.java
@@ -0,0 +1,55 @@
+package personnages;
+
+public class Humains {
+	private String nom;
+	private String boissonFavourite;
+	private int qtargent;
+	
+	public Humains(String nom, String boissonFavourite, int qtargent) {
+		this.nom = nom;
+		this.boissonFavourite = boissonFavourite;
+		this.qtargent = qtargent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getQtargent() {
+		return qtargent;
+	}
+	
+	private void parler(String texte) {
+		System.out.println("(" + nom + ") - " + texte);	
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boissonFavourite + "." );
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boissonFavourite + " ! GLOUPS !");
+	}
+	
+	
+	private void gagnerArgent(int gain) {
+		qtargent += gain;
+	}
+	
+	private void perdreArgent(int gain) {
+		qtargent -= gain;
+		if (qtargent < 0) {
+			qtargent = 0;
+		}
+	}
+	
+	
+	public void acheter(String bien, int prix) {
+		if (prix <= qtargent) {
+			parler("J'ai de l'argent, je peux m'offrir " + bien + ".");
+			perdreArgent(prix);
+		} else {
+			parler("J'ai pas d'argent suffissant pour m'acheter " + bien + ".");
+		} 
+	}
+}
